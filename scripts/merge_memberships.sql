921050f7009a7fff5f695b928999547511bb228c
Forest Gregg <fgregg@datamade.us>
2023-01-11 14:39:30 -0500

    delete the right one

--- scripts/merge_memberships.sql
+++ scripts/merge_memberships.sql
@@ -15,6 +15,6 @@ DELETE FROM opencivicdata_membership AS A USING opencivicdata_membership AS B
 WHERE A.role = B.role
     AND A.person_name = B.person_name
     AND A.organization_id = B.organization_id
-    AND A.start_date < B.start_date
-    AND A.end_date > B.start_date;
+    AND A.start_date > B.start_date
+    AND A.end_date <= B.start_date;
 
