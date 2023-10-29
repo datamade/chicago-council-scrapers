c97c64e89cac5857d4730fcfae5d86e3f5d64b6f
Forest Gregg <fgregg@datamade.us>
2023-10-28 22:27:24 -0400

    tweak

--- scripts/bad_introductions.sql
+++ scripts/bad_introductions.sql
@@ -1,22 +1,27 @@
-COPY ( SELECT DISTINCT ON (opencivicdata_bill.id)
-        identifier,
-        title,
-    LEFT (opencivicdata_bill.extras ->> 'introduction_str',
-        10) AS introduction_date,
-    date AS earliest_action_date,
-    opencivicdata_bill.extras ->> 'matter_id' AS matter_id,
-    opencivicdata_bill.extras ->> 'introduction_failure_mode' AS "problem"
-FROM
-    opencivicdata_bill
+COPY (
+    SELECT
+        *
+    FROM ( SELECT DISTINCT ON (opencivicdata_bill.id)
+            identifier,
+            title,
+        LEFT (opencivicdata_bill.extras ->> 'introduction_str',
+            10) AS introduction_date,
+        date AS earliest_action_date,
+        opencivicdata_bill.extras ->> 'matter_id' AS matter_id,
+        opencivicdata_bill.extras ->> 'introduction_failure_mode' AS "problem"
+    FROM
+        opencivicdata_bill
     LEFT JOIN opencivicdata_billaction ON bill_id = opencivicdata_bill.id
 WHERE
     opencivicdata_bill.extras ->> 'introduction_failure_mode' IS NOT NULL
 ORDER BY
     opencivicdata_bill.id,
-    date
+    date) t
 ORDER BY
     identifier)
-TO STDOUT WITH CSV HEADER;
+    TO STDOUT WITH CSV HEADER;
+
+
 
 
 
