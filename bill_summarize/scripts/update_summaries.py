21b32662a7476d0be5ac0abc2c95d4ffea7f78a6
Forest Gregg <fgregg@datamade.us>
2024-03-18 11:36:12 -0400

    fmt

--- bill_summarize/scripts/update_summaries.py
+++ bill_summarize/scripts/update_summaries.py
@@ -8,24 +8,16 @@ updated_files = set()
 with open(original_file) as f:
     reader = csv.DictReader(f)
     for row in reader:
-        updated_files.add(row['filename'])
+        updated_files.add(row["filename"])
 
 writer = csv.DictWriter(sys.stdout, fieldnames=["filename", "summary"])
 writer.writeheader()
 with open(original_file) as f:
     reader = csv.DictReader(f)
     for row in reader:
-        if row['filename'] not in updated_files:
+        if row["filename"] not in updated_files:
             writer.writerow(row)
 
 with open(second_file) as f:
     reader = csv.DictReader(f)
     writer.writerows(reader)
-
-
-        
-                
-
-
-
-        
