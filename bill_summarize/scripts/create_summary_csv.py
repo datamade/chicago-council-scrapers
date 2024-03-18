e556db64f3f7f87fb011f51d4cdb4aec00086a9f
Forest Gregg <fgregg@datamade.us>
2024-03-18 11:41:43 -0400

    simplify

--- bill_summarize/scripts/create_summary_csv.py
+++ bill_summarize/scripts/create_summary_csv.py
@@ -7,6 +7,5 @@ writer.writeheader()
 
 for summary_file in pathlib.Path("summaries").glob("*.summary"):
     summary = summary_file.open().read().strip()
-    if summary != "TEXT IS EMPTY":
-        row = {"filename": summary_file.stem, "summary": summary}
-        writer.writerow(row)
+    row = {"filename": summary_file.stem, "summary": summary}
+    writer.writerow(row)
