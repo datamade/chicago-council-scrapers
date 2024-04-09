36435aa5e85ac180dd4521311e65c15e262bdc13
Forest Gregg <fgregg@datamade.us>
2024-04-09 09:52:25 -0400

    update summaries sql script

--- bill_summarize/scripts/update_summaries.sql
+++ bill_summarize/scripts/update_summaries.sql
@@ -13,22 +13,11 @@ FROM
 
 WITH summaries as (
 SELECT
-    version_id, jsonb_build_object("summary", summary) as summary
+    version_id, jsonb_build_object('summary', summary) as summary
 FROM
     bill_summaries
     INNER JOIN opencivicdata_billversionlink ON url LIKE '%' || filename)
 UPDATE opencivicdata_billversion SET extras = extras || summary
 FROM summaries WHERE opencivicdata_billversion.id = version_id;
-
-
-WITH summaries as (
-SELECT
-    bill_id, jsonb_build_object("summary", summary) as summary
-FROM
-    bill_summaries
-    INNER JOIN opencivicdata_billversionlink ON url LIKE '%' || filename
-    INNER JOIN opencivicdata_billversion ON opencivicdata_billversion.id = version_id)
-UPDATE opencivicdata_bill SET extras = extras || summary
-FROM summaries WHERE opencivicdata_bill.id = bill_id;
 
 COMMIT;
