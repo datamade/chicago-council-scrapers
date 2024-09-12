2c0d378ac6f7e864fdecfa96f3659c71e386cde5
Forest Gregg <fgregg@datamade.us>
2024-09-11 21:18:18 -0400

    create summaries

--- bill_summarize/scripts/create_summary_csv.py
+++ bill_summarize/scripts/create_summary_csv.py
@@ -1,11 +1,13 @@
 import csv
 import pathlib
 import sys
+import json
 
 writer = csv.DictWriter(sys.stdout, fieldnames=["filename", "summary"])
 writer.writeheader()
 
 for summary_file in pathlib.Path("summaries").glob("*.summary"):
-    summary = summary_file.open().read().strip()
-    row = {"filename": summary_file.stem, "summary": summary}
+    with summary_file.open() as f:
+        summary_dict = json.load(f)
+    row = {"filename": summary_file.stem, "summary": summary_dict["summary"]}
     writer.writerow(row)
